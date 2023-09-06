a1188fe4c9c0016e184e18905ad42dbe9cdb2cb9
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-09-06 19:45:26 +0000

    basic random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// const btn = document.getElementById("btn");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardNum = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const newCardNumber = document.createElement("h1");
+  newCardNumber.innerHTML = cardNum[Math.floor(Math.random() * 12)];
+  document.querySelector(".number").appendChild(newCardNumber);
+
+  const suit = ["heart", "spade", "club", "diamond"];
+  const newSuit = suit[Math.floor(Math.random() * 4)];
+  const top = document.querySelector(".header");
+  top.classList.add(newSuit);
+
+  const bottom = document.querySelector(".footer");
+  bottom.classList.add(newSuit);
 };
