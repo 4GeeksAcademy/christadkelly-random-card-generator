5f4284de40c68aafe0147b22d42d21d143a347fa
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-09-06 20:26:43 +0000

    generate new card with button

--- src/app.js
+++ src/app.js
@@ -5,19 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// const btn = document.getElementById("btn");
+window.onload = generateNewCard();
 
-window.onload = function() {
+function generateNewCard() {
   const cardNum = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
-  const newCardNumber = document.createElement("h1");
-  newCardNumber.innerHTML = cardNum[Math.floor(Math.random() * 12)];
-  document.querySelector(".number").appendChild(newCardNumber);
+  const cardNumber = document.getElementById("cardNumber");
+  cardNumber.innerHTML = cardNum[Math.floor(Math.random() * 12)];
 
   const suit = ["heart", "spade", "club", "diamond"];
   const newSuit = suit[Math.floor(Math.random() * 4)];
+
   const top = document.querySelector(".header");
-  top.classList.add(newSuit);
+  top.className = "";
+  top.classList.add("header", newSuit);
 
   const bottom = document.querySelector(".footer");
-  bottom.classList.add(newSuit);
-};
+  bottom.className = "";
+  bottom.classList.add("footer", newSuit);
+}
+
+const btn = document.getElementById("button");
+btn.addEventListener("click", generateNewCard);
